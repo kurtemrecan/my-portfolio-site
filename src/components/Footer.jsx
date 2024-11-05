0788c0305c0e68f4e33fe57c6d07630cdb4bf5c8
Emre Can Kurt <can.kurt764@gmail.com>
2024-11-05 22:06:32 +0300

    add media query

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ export default function Footer() {
     <div className="footer bg-gray-100 p-12 dark:bg-dark-gray">
       <div className="container flex flex-col items-start">
         <div className="text-left">
-          <p className="text-4xl font-regular dark:text-custom-gray mt-8 mb-12">
+          <p className="text-2xl sm:text-4xl font-regular dark:text-custom-gray mt-8 mb-12">
             Let's work together on <br /> your next product
           </p>
         </div>
-        <div className="flex items-center justify-between w-full mt-4">
-          <p className="flex items-center">
+        <div className="flex flex-col sm:flex-row items-center justify-between w-full mt-4">
+          <p className="flex items-center mb-4 sm:mb-0">
             <span
               role="img"
               aria-label="right pointing finger"
@@ -25,14 +25,14 @@ export default function Footer() {
           <div className="flex space-x-4">
             <a
               href="#"
-              className="text-black dark:text-custom-indigo-lightest font-regular"
+              className="text-black dark:text-custom-indigo-lightest font-regular text-base sm:text-lg"
             >
               Personal Blog
             </a>
-            <a href="#" className="text-custom-green">
+            <a href="#" className="text-custom-green text-base sm:text-lg">
               GitHub
             </a>
-            <a href="#" className="text-custom-blue">
+            <a href="#" className="text-custom-blue text-base sm:text-lg">
               LinkedIn
             </a>
           </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,27 +11,27 @@ const Hero = () => {
   };
 
   return (
-    <section className="container flex items-center justify-between p-8 text-white mt-12">
+    <section className="container flex flex-col sm:flex-row items-center justify-between p-8 text-white mt-12">
       <div className="flex flex-col">
         <div className="flex items-center mb-2 mt-12">
           <div className="border-t-2 border-custom-blue dark:border-custom-indigo-light w-32"></div>
-          <h1 className="mx-4 text-xl font-regular text-indigo-700 dark:text-custom-indigo-light">
+          <h1 className="mx-4 text-lg sm:text-xl md:text-2xl font-regular text-indigo-700 dark:text-custom-indigo-light">
             Emre Can Kurt
           </h1>
         </div>
-        <h2 className="text-8xl font-bold text-gray-800 dark:text-custom-gray mt-8 mb-12">
+        <h2 className="text-4xl sm:text-6xl md:text-8xl font-bold text-gray-800 dark:text-custom-gray mt-8 mb-12">
           Creative thinker, minimalism lover
         </h2>
-        <p className="text-xl mb-6 text-gray-400 dark:text-white mb-8">
+        <p className="text-base sm:text-lg md:text-xl mb-6 text-gray-400 dark:text-white mb-8">
           Hi, I'm Emre Can Kurt. I'm a full-stack developer. If you are looking
           for a <br /> Developer who to craft solid and scalable frontend
           products with <br /> great use experiences. Let's shake hands with me.
         </p>
-        <div className="flex space-x-4 mb-12">
+        <div className="flex flex-col sm:flex-row space-x-0 sm:space-x-4 mb-12">
           <a
             href="mailto:can.kurt764@gmail.com"
             onClick={() => handleButtonClick('hire')}
-            className={`border border-custom-blue dark:border-custom-indigo-lightest dark:hover:bg-gray-600  py-2 px-4 rounded transition ${
+            className={`border border-custom-blue dark:border-custom-indigo-lightest dark:hover:bg-gray-600 py-2 px-4 rounded transition ${
               clickedButton === 'hire'
                 ? 'bg-custom-blue text-white dark:bg-custom-indigo-lightest dark:text-black'
                 : 'text-indigo-700 dark:text-custom-indigo-lightest dark:bg-custom-light-gray'
@@ -43,7 +43,7 @@ const Hero = () => {
             href="https://github.com/kurtemrecan"
             target="_blank"
             onClick={() => handleButtonClick('github')}
-            className={`flex items-center border border-custom-blue dark:border-custom-indigo-lightest dark:hover:bg-gray-600  py-2 px-4 rounded transition ${
+            className={`flex items-center border border-custom-blue dark:border-custom-indigo-lightest dark:hover:bg-gray-600 py-2 px-4 rounded transition ${
               clickedButton === 'github'
                 ? 'bg-custom-blue text-white dark:bg-custom-indigo-lightest dark:text-black'
                 : 'text-indigo-700 hover:bg-custom-blue hover:text-white dark:text-custom-indigo-lightest dark:bg-custom-light-gray'
@@ -82,7 +82,7 @@ const Hero = () => {
       <img
         src={MyImage}
         alt="profile-photo"
-        className="rounded-lg shadow-4xl"
+        className="rounded-lg shadow-4xl w-full sm:w-1/2 md:w-1/3"
       />
     </section>
   );
